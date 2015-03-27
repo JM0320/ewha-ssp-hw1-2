80ec145bd48a03e734cb9f45dc72ed97a8ead4bf
JM0320 <zxij@hanmail.net>
2015-03-27 16:11:52 +0900

    change file lab2_1
    change

--- lab2_1/lab2_1.cpp
+++ lab2_1/lab2_1.cpp
@@ -2,42 +2,35 @@
 #include "lab2_1.h"
 #define FLED 10
 #define RLED 9
+int Rvalue=100;
 
+void myAnalogWrite (int pin, int percent, int time){
+	int i=0;
+	for(i; i<(time/duty); i++){
+		digitalWrite(pin, HIGH);
+		delay(percent*duty/100);
+		digitalWrite(pin, LOW);
+		delay(((100-percent)*duty)/100);
+	}
+}
 
 //The setup function is called once at startup of the sketch
 void setup(){
-	pinMode(FLED, OUTPUT);
 	pinMode(RLED, OUTPUT);
 }
 
 // The loop function is called in an endless loop
 void loop(){
 	int i;
-	int Fvalue=0, Rvalue=255;
 
-	for(i=0; i<7; i++){
-		analogWrite(FLED, Fvalue);
-		analogWrite(RLED, Rvalue);
-		delay(625);
-		Fvalue+=30;
-		Rvalue-=30;
+	while(Rvalue>0){
+		myAnalogWrite(RLED, Rvalue, 1000);
+		Rvalue-=10;
 	}
-	Fvalue=255;
-	Rvalue=0;
-	analogWrite(FLED, Fvalue);
-	analogWrite(RLED, Rvalue);
-	delay(625);
 
-	for(i=0; i<7; i++){
-		analogWrite(FLED, Fvalue);
-		analogWrite(RLED, Rvalue);
-		delay(625);
-		Fvalue-=30;
-		Rvalue+=30;
+	if(Rvalue==0){
+		while(1){
+			digitalWrite(RLED, LOW);
+		}
 	}
-	Fvalue=0;
-	Rvalue=255;
-	analogWrite(FLED, Fvalue);
-	analogWrite(RLED, Rvalue);
-	delay(625);
 }
